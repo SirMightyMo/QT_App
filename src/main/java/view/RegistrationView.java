6c001c56b83f4d30baac51c7b848c8ef89e8ca94
Sven M <sven.Maderer@haw-hamburg.de>
2022-01-18 20:02:56 +0100

    Update RegistrationView.java

--- src/main/java/view/RegistrationView.java
+++ src/main/java/view/RegistrationView.java
@@ -11,6 +11,7 @@ import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.JPasswordField;
 import javax.swing.JPanel;
+import java.awt.Rectangle;
 
 
 
@@ -100,6 +101,7 @@ public final class RegistrationView extends JFrame {
 		panelTwo.add(securityQuestionLabel, BorderLayout.NORTH);
 		
 		securityQuestionPicker = new JComboBox(questions);
+		securityQuestionPicker.setBounds(100, 20, 165, 25);
 		panelTwo.add(securityQuestionPicker, BorderLayout.CENTER);
 		
 		
@@ -123,11 +125,11 @@ public final class RegistrationView extends JFrame {
 		
 		
 		this.setTitle("Register");
-		this.setLayout(new GridLayout(4,1));
-		this.add(panel);
-		this.add(panelTwo);
-		this.add(panelThree);
-		this.add(errorMessage);
+		getContentPane().setLayout(new GridLayout(4,1));
+		getContentPane().add(panel);
+		getContentPane().add(panelTwo);
+		getContentPane().add(panelThree);
+		getContentPane().add(errorMessage);
 		this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
 		this.pack();
 		this.setVisible(true);
