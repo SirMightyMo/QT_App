073dc7f7245d668b4038fca3232a6590402c89ed
LNDR <66724827+LNDR-WST@users.noreply.github.com>
2022-02-04 15:09:59 +0100

    removed unused imports

--- src/main/java/MainMethod.java
+++ src/main/java/MainMethod.java
@@ -1,16 +1,7 @@
 package main.java;
 
-import javax.swing.UIManager;
-
-import com.formdev.flatlaf.FlatDarkLaf;
-import com.formdev.flatlaf.FlatLightLaf;
 import main.java.controller.DatabaseController;
 import main.java.controller.LayoutManager;
-import main.java.controller.DashboardController;
-import main.java.controller.ProjectController;
-import main.java.controller.TestHourEntryController;
-import main.java.controller.TimerHourController;
-import main.java.view.TimerView;
 import main.java.controller.LoginController;
 
 public class MainMethod {
